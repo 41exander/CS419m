ed7aca0b83979296e8701a2e59d97c4d3646a1fb
dhanvi97 <dhanvi.sreeni@gmail.com>
2018-08-04 12:12:07 +0530

    Sorted Attribute Lists, along with comments explaining the functions

--- HW1/regression_tree.py
+++ HW1/regression_tree.py
@@ -1,5 +1,11 @@
 import csv
 
+######################################################################################################
+# This method creates the dataset from the train.csv file
+# Headers are removed to make further calculations easier
+# Also appending each record with the resource ID for SPRINT implementation
+######################################################################################################
+
 def inputDataset(file_path):
 	f = open(file_path, 'rb')
 	data = list(csv.reader(f))
@@ -10,16 +16,32 @@ def inputDataset(file_path):
 		data[i].append(i)
 	return data
 
+
+######################################################################################################
+# This method generates attribute lists from the data, need to implement sorting
+# The attrribute lists are stored as a 3D array, A[i][j][k]
+# i -> attribute index (among Xi's)
+# j -> record index 
+# k -> value index (there will be only 3 values in this, the attribute value, output and RID)
+# Finally, each of these attrbiute lists is sorted so as to increase computation efficiency
+######################################################################################################
+
 def createAttrLists(data):
 	record_count = len(data)
 	attribute_count = len(data[0])-2
 	attr_list = [[[0 for x in range(3)] for y in range(record_count)] for z in range(attribute_count)]
+	sorted_attr_list = [[[0 for x in range(3)] for y in range(record_count)] for z in range(attribute_count)]
 	for attr in range(attribute_count):
 		for record in xrange(record_count):
 			attr_list[attr][record][0] = float(data[record][attr])
 			attr_list[attr][record][1] = float(data[record][attribute_count])
 			attr_list[attr][record][2] = int(data[record][attribute_count+1])
-	return attr_list
+		sorted_attr_list[attr] = sortAttrList(attr_list[attr])
+	return sorted_attr_list
+
+def sortAttrList(attr_list):
+	sorted_attr_list = sorted(attr_list, key=lambda item:item[0])
+	return sorted_attr_list
 
 def GrowTree(D):
 	Partition(D)
@@ -37,4 +59,3 @@ if __name__ == '__main__':
 	
 	dataset = inputDataset('train.csv')
 	attr_list = createAttrLists(dataset)
-	print attr_list[0][0]
